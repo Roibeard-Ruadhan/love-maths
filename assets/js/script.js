c1db4736dfd83c7d2fa103e46d03d6b70325c0cb
Roibéard R Coughlan <roibeard.c@outlook.com>
2021-08-23 20:52:44 +0000

    Add code to check answer & respond to user

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,10 +37,29 @@ document.addEventListener("DOMContentLoaded", function() {
 
 }
 
-function checkAnswer() {
+/**
+ * Checks the answer agaist the first element in
+ * the returned calculateCorrectAnswer array
+ */
+ function checkAnswer() {
+
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+
+    if (isCorrect) {
+        alert("Hey! You got it right! :D");
+    } else {
+        alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+    }
+
+    runGame(calculatedAnswer[1]);
 
 }
-
+/**
+ * Gets the operands (the numbers) and the operator (plus, minus etc)
+ * directly from the dom, and returns the correct answer.
+ */
 function calculateCorrectAnswer() {
 
     let operand1 = parseInt(document.getElementById('operand1').innerText);
